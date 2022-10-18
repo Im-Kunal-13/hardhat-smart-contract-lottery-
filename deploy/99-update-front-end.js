da9c69ac9b5efa395e8b0177b182d318ae48a360
ImKunal13z <2002kunalmondal13@gmail.com>
2022-10-18 15:37:01 +0530

    Initial Commit

---
+++ deploy/99-update-front-end.js
@@ -0,0 +1,33 @@
+const { ethers, network } = require("hardhat")
+const fs = require("fs")
+
+const FRONT_END_ADDRESSES_FILE = "../next-js-smart-contract-lottery-fcc/constants/contractAddresses.json"
+const FRONT_END_ABI_FILE = "../next-js-smart-contract-lottery-fcc/constants/abi.json"
+
+module.exports = async function () {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("Updating front end...")
+        await updateContractAddresses()
+        await updateAbi()
+    }
+}
+
+async function updateContractAddresses() {
+    const raffle = await ethers.getContract("Raffle")
+    const contractAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8"))
+    if (network.config.chainId.toString() in contractAddresses) {
+        if (!contractAddresses[network.config.chainId.toString()].includes(raffle.address)) {
+            contractAddresses[network.config.chainId.toString()].push(raffle.address)
+        }
+    } else {
+        contractAddresses[network.config.chainId.toString()] = [raffle.address]
+    }
+    fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(contractAddresses))
+}
+
+async function updateAbi() {
+    const raffle = await ethers.getContract("Raffle")
+    fs.writeFileSync(FRONT_END_ABI_FILE, raffle.interface.format(ethers.utils.FormatTypes.json))
+}
+
+module.exports.tags = ["all", "frontend"]
